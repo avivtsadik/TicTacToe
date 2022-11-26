2b7233d2adcd55cb07ae0113b8270be0b5c21838
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2022-11-26 16:40:37 +0200

    add on click listener

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         GridLayout gridLayout = findViewById(R.id.gridLayout);
         gridLayout.setBackgroundResource(R.drawable.back);
+
+        int childCount = gridLayout.getChildCount();
+
+        for (int i= 0; i < childCount; i++){
+            ImageView imageView = (ImageView) gridLayout.getChildAt(i);
+            imageView.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View view){
+
+                }
+            });
+        }
     }
 }
