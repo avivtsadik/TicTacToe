70581c75e7a24b405b098f1f556e97d711270833
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2022-11-26 16:36:45 +0200

    add grid images

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+        gridLayout.setBackgroundResource(R.drawable.back);
     }
 }
